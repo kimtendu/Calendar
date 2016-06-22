2004c70fbed1abcb3fda4f409d6b2531238b5aa7
kimtendu <kimtendu@gmail.com>
2016-06-22 16:33:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
 function jCalendar (options){
+	
+	//Default options
     var elem, ArrDayInMonth = [];
-
-
-
     if (!options.startDate) options.startDate = new Date;
     if (!options.target) options.target = 'jCalendar';
 
     //here im starting to build a calendar
     var $dateYear = options.startDate.getFullYear();
+	
     var $dateMonth = options.startDate.getMonth();
     var $dateDay = options.startDate.getDate();
-
-
-
+	
     for (i=0; i<12; i++){
         var j = dayInMonth(i);
         ArrDayInMonth[i] = j;
     }; // ArrDayInMonth - in that arr i know max numbers of day in all month of that year
 
    // console.log (ArrDayInMointh);
-    printMonth($dateMonth);
+    //printMonth($dateMonth);
+	
+	
+	//Controllers
+	
+	 this.printMonth = function(month){
+		if (!month) month = options.startDate.getMonth();
+		 
+		 
+		
+		var $getDay = new Date($dateYear, month, 1).getDay();
+		//console.log (' $getDay - '+ $getDay);
+		var $div = document.createElement('div');
+        $div.className='row';
+        var $monthNum = document.createElement('div');
+        $monthNum.className='col-md-7';
+        //$monthNum.innerHTML = ArrDayInMonth[month];// month number and not numbers of days in month
+		 $monthNum.innerHTML = '<p id="prev">prev</p>' + options.title + '<p id="next">next</p>';
+        $div.appendChild($monthNum);
+		 
+		for ($i = $getDay-1; $i >= 0; $i--){
+			var m = month-1;
+			var j = ArrDayInMonth[m]-$i;
+            var $out = printDay(j);
+            $div.appendChild($out);
+        } 
+        for ($i = 1; $i<= ArrDayInMonth[month]; $i++){
+            var $out = printDay($i, $dateDay);
+            $div.appendChild($out);
+        }
+		 		 
+		var $getThisMonthDay = new Date($dateYear, month, ArrDayInMonth[month]).getDay(); 
+		var $afday = 1;
+		for ($i = $getThisMonthDay; $i< 6; $i++){
+            var $out = printDay($afday);
+            $div.appendChild($out);
+			$afday++;
+        }
+		 
+		 
+        var $calendar = document.getElementById(options.target);
+		$calendar.innerHTML='';
+        $calendar.appendChild($div);
 
+    };//here i can print the month
+	
+	
+
+	//End controllers
+	
+	
+	
+	//Functions
 
 
     function dayInMonth(month){
         return 33 -  new Date($dateYear, month, 33).getDate();
     }; //here i can get a numbers of day in the year
-    function printMonth(month){
-        var $div = document.createElement('div');
-        $div.className='row';
-        var $monthNum = document.createElement('div');
-        $monthNum.className='col-md-7';
-        $monthNum.innerHTML = ArrDayInMonth[month];// month number and not numbers of days in month
-        $div.appendChild($monthNum);
-        for ($i = 1; $i<= ArrDayInMonth[month]; $i++){
-            var $out = printDay($i, $dateDay);
-            $div.appendChild($out);
-        }
-        var $calendar = document.getElementById(options.target);
-        $calendar.appendChild($div);
-
-    };//here i can print the month
+   
 
     function printDay($dateDay, $dateToday){
         var div = document.createElement('div');
@@ -49,9 +84,27 @@ function jCalendar (options){
         return div;
     } //func that print the days. $dateDay - this date input. $dateToday - if its in shedule.
 
+	
+	
+	//Init
+	
+	this.printMonth($dateMonth);
+	
+	var elemPrv = document.getElementById('prev');
+	elemPrv.onclick = function(Event){
+		$dateMonth--;
+		this.printMonth($dateMonth);
+		
+	};
+	
+	var elemNext = document.getElementById('next');
+	elemNext.onclick = function(Event){
+		$dateMonth++;
+		printMonth($dateMonth);
+		
+	}
+
 }
 
 
-var myfirstCalendar = new jCalendar({
-    title: "Сладости"
-});
+
