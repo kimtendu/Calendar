a6087d05b9698eb194e3c9faaef94d7665221814
kimtendu <kimtendu@gmail.com>
2016-06-29 16:37:32 +0300

    Add files via upload
    added Grid ans events on grid

--- index.js
+++ index.js
@@ -17,34 +17,41 @@ function jCalendar (options){
     }; // ArrDayInMonth - in that arr i know max numbers of day in all month of that year
 	
 	var $calendar = document.getElementById(options.target);
+	var divHeader = document.createElement('div');
+	$calendar.appendChild(divHeader);
 	var $monthdiv = document.createElement('div');
+	$monthdiv.classList.add(options.tpl.grid.TplClass);
 	$calendar.appendChild($monthdiv);
 
-  	
-	//Controllers
+  	//End Start Data
 	
-	this.printControllers = function()
-	{
-		var controllers = document.createElement('div');
-		controllers.innerHTML = '<p id="prev">prev</p> <p id="next">next</p>';
-		$calendar.appendChild(controllers);
-		
-	}
 	
-	this.printMonth = function(month){
-		if (!month) month = options.startDate.getMonth();
+	//Class
+	
+	this.printHeader = function(){		
+		divHeader.classList.add(options.tpl.header.class);
+		if (options.tpl.header.nextButton){
+			 createElementAndPush('next', 'next', divHeader )
+		}
+		if (options.tpl.header.prevButton){
+			createElementAndPush('prev', 'prev', divHeader )
+		}		
+		if (options.title){
+			createElementAndPush('title', options.title, divHeader)
+		}
 				
-		var $getDay = new Date($dateYear, month, 1).getDay();
-		//console.log (' $getDay - '+ $getDay);
+	}//print header of Calendar
+	
+	this.printGrid = function(month, view){
+		if (!month) month = options.startDate.getMonth();
+		if (!view) view = options.view.calendarView;
+		
 		var $div = document.createElement('div');
         $div.className='row';
-        var $monthNum = document.createElement('div');
-        $monthNum.className='col-md-7';
-		new Date($dateYear, month, 1).format('MMM');
-        //$monthNum.innerHTML = ArrDayInMonth[month];// month number and not numbers of days in month
-		 $monthNum.innerHTML = options.title ;
-        $div.appendChild($monthNum);
-		 
+		$div.id="grid";
+		
+		var $getDay = new Date($dateYear, month, 1).getDay();
+		if (view=="month"){		
 		for ($i = $getDay-1; $i >= 0; $i--){
 			var m = month-1;
 			var j = ArrDayInMonth[m]-$i;
@@ -52,10 +59,9 @@ function jCalendar (options){
             $div.appendChild($out);
         } 
         for ($i = 1; $i<= ArrDayInMonth[month]; $i++){
-            var $out = printDay($i, $dateDay);
+            var $out = printDay($i, $dateDay, month);
             $div.appendChild($out);
-        }
-		 		 
+        }		 		 
 		var $getThisMonthDay = new Date($dateYear, month, ArrDayInMonth[month]).getDay(); 
 		var $afday = 1;
 		for ($i = $getThisMonthDay; $i< 6; $i++){
@@ -63,52 +69,89 @@ function jCalendar (options){
             $div.appendChild($out);
 			$afday++;
         }
-		 
+		
+			
+		}//Month view
+		else{
+			console.log('here will be weeks view');
+		}//week view
+		
 		 
         $monthdiv.innerHTML='';
         $monthdiv.appendChild($div);
 		
-
-    };//here i can print the month
-	
-	
-	this.getJsonSh = function(schedule){
-		options.events = JSON.parse(schedule, function(key, value) {
-		  if (key == 'date') return new Date(value);
-		  return value;
-		});
-		console.log( options.events );
+		parseJsonToGrid(options.contents);
 		
-	}
+		
+	}//print main Grid of calendar
 	
 
-	//End controllers
+	
+	//End Class
 	
 	
 	
 	//Functions
-
+	function createElementAndPush($id, $text, $parent){
+		var $div = document.createElement('div');
+			$div.id = $id;
+			$div.innerHTML = $text
+			$parent.appendChild($div);		
+	}
+		
 
     function dayInMonth(month){
         return 33 -  new Date($dateYear, month, 33).getDate();
     }; //here i can get a numbers of day in the year
    
 
-    function printDay($dateDay, $dateToday){
-        var div = document.createElement('div');
+    function printDay($dateDay, $dateToday, $month){
+		var div = document.createElement('div');
         ($dateDay == $dateToday) ? div.className = 'col-md-1 today' : div.className = 'col-md-1';
+		div.setAttribute('data-day', $dateDay);
+		div.setAttribute('data-month', $month);
         div.innerHTML= $dateDay;
         return div;
     } //func that print the days. $dateDay - this date input. $dateToday - if its in shedule.
-
 	
+	function parseJsonToGrid($contents){
+		var $gridsElements = document.querySelectorAll('#grid .col-md-1');
+		
+		for ( var i = 0; i < $gridsElements.length; i++){
+			var $element = $gridsElements[i];			
+			for ( var $obj in $contents) {
+				
+				var $startdate = new Date($contents[$obj].startdate).getDate(),
+					$monthdate = new Date($contents[$obj].startdate).getMonth(),
+					$elementStartdate = $element.getAttribute('data-day'),
+					$elementMonth = $element.getAttribute('data-month');
+				if ($elementStartdate == $startdate && $monthdate == $elementMonth){
+					var $oldInner =  $element.innerHTML
+					$element.innerHTML= $oldInner + "here Json out";
+					//console.log('here JSON');
+				}
+				
+			
+			}
+			
+		}
+		
+	}; // set all ivents to grid in Montth view
+	
+
+	//end functions
 	
 	//Init
-	this.printControllers();
-	this.printMonth($dateMonth);
+	//this.printControllers();
+	this.printHeader();
+	this.printGrid($dateMonth);
 	
-	this.getJsonSh(options.events);
+	//his.getJsonSh(options.contents);
+	//parseContents(options.contents);
+	//console.log(options.contents);
 	
+	
+	//Controllers
 	var elemPrv = document.getElementById('prev');
 	elemPrv.onclick = function(Event){
 		$dateMonth--;
@@ -122,8 +165,8 @@ function jCalendar (options){
 		elem.printMonth($dateMonth);
 		
 	}
+	
+	//End controllers
+	
 
 }
-
-
-
